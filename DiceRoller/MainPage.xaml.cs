09da3d3eae1cb8a1095657d3b3f274f4fc1338db
cassiafm <cassiafmolinaro@gmail.com>
2024-06-22 16:44:58 -0300

    Adicionar arquivos de projeto.

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RollDiceButton_Clicked(object sender, EventArgs e)
+        {
+
+            int selectedSides = Convert.ToInt32(SidesPicker.SelectedItem);
+
+            int drawnNumber = new Random().Next(1, selectedSides + 1);
+
+            ResultLabel.Text = drawnNumber.ToString();
+        }
+    }
+
+}
